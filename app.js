90af8b472c8062c5d94319c49420615d54ef623e
Part-Time <part-time@MacBook-Air-3.local>
2020-03-07 15:08:24 -0600

    Feature: Add React bundle

--- app.js
+++ app.js
@@ -41,12 +41,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
       
 
 app.set('views', path.join(__dirname, 'views'));
@@ -86,6 +80,9 @@ app.use('/api', authRoutes);
 app.use('/api', require('./routes/project-routes'));
 app.use('/api', require('./routes/task-routes'));
 
-
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 module.exports = app;
---
+++ public/precache-manifest.c4a055676f3ea48297523c52184c9af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a6efd6ccab0c8c817358f566393db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72cffc9d1de1bb26eb51",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77e3b4fe0167695969c1",
+    "url": "/static/js/2.6afe9dc5.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.6afe9dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cffc9d1de1bb26eb51",
+    "url": "/static/js/main.e5baa36d.chunk.js"
+  },
+  {
+    "revision": "be51c232505d6ce02f19",
+    "url": "/static/js/runtime-main.ea4104cd.js"
+  }
+]);
